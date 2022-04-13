09770f7598a15be30129d3ad71d6d9fe87b98d75
David Castellar Martínez <castellardav@gmail.com>
2022-04-13 16:42:45 -0500

    Reformat backend files

---
+++ backend/routes/auth/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const google = require("./google");
+
+router.post("/login", (req, res) => {
+  console.log("New request POST to /login");
+
+  console.log(req.body);
+
+  const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
+
+  let data = {
+    success: true,
+    message: `User ${req.body.email} registered correctly`,
+    token: token,
+    data: req.body,
+  };
+
+  res.json(data);
+});
+
+router.get("/failed", (req, res) => res.send("Hay un error en el login"));
+
+router.use("", google);
+
+module.exports = router;
