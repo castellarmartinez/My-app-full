4807448cd7f88afdb3e94841d23b22483ef583a9
David Castellar Martínez <castellardav@gmail.com>
2022-04-13 16:25:27 -0500

    Adds routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,18 @@
+require('./services')
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const passport = require('passport');
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-// Add headers before the routes are defined
 app.use(cors());
-
-// Parse JSON bodies (as sent by API clients)
+app.use(passport.initialize());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("New request GET to /");
-  res.send("Hola Mundo!");
-});
-
-app.post("/login", (req, res) => {
-  console.log("New request POST to /login");
-  console.log(req.body);
-
-  const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
-
-  let data = {
-    success: true,
-    message: `User ${req.body.email} registered correctly`,
-    token: token,
-    data: req.body,
-  };
-
-  res.json(data);
-});
+app.use(require('./routes/public'));
+app.use(require('./routes/auth'));
 
 app.listen(PORT, () => {
-  console.log(`App listening the port [${PORT}]!`);
+  console.log(`App is running on PORT ${PORT}!`);
 });
