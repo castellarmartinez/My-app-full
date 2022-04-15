87f91bef7c2a2c2ffd819d0b3a8e9a8f7d752ad4
David Castellar Martínez <castellardav@gmail.com>
2022-04-15 14:41:59 -0500

    Requires modules to allow login with GitHub

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const google = require("./google");
 const facebook = require("./facebook");
 const linkedin = require("./linkedin");
+const github = require("./github");
 
 router.post("/login", (req, res) => {
   console.log("New request POST to /login");
@@ -26,5 +27,6 @@ router.get("/failed", (req, res) => res.send("Hay un error en el login"));
 router.use("", google);
 router.use("", facebook);
 router.use("", linkedin);
+router.use("", github);
 
 module.exports = router;
