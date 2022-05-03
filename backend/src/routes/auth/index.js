0740576b2f562e4b3a7d644f0bc07f8232881356
David Castellar Martínez <castellardav@gmail.com>
2022-05-02 22:19:16 -0500

    Removes unused endpoint auth/login

--- backend/src/routes/auth/index.js
+++ backend/src/routes/auth/index.js
@@ -5,23 +5,6 @@ const facebook = require("./facebook");
 const linkedin = require("./linkedin");
 const github = require("./github");
 
-router.post("/login", (req, res) => {
-  console.log("New request POST to /login");
-
-  console.log(req.body);
-
-  const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
-
-  let data = {
-    success: true,
-    message: `User ${req.body.email} registered correctly`,
-    token: token,
-    data: req.body,
-  };
-
-  res.json(data);
-});
-
 router.get("/failed", (req, res) => res.send("Hay un error en el login"));
 
 router.use("", google);
