49c170d2f885d1133b4466156d9d8f76a320422b
David Castellar Martínez <castellardav@gmail.com>
2022-05-03 22:53:11 -0500

    Allows to login with google

--- backend/src/routes/auth/google.js
+++ backend/src/routes/auth/google.js
@@ -22,28 +22,32 @@ router.get(
     failureRedirect: "/failed",
   }),
   async (req, res) => {
-    console.log(`Peticion get /${strategyName}/callback`);
-
-    let data = {};
-
-    data.name = req.user.displayName;
-    data.username = "google" + req.user.id.slice(1, 7);
-    data.password = req.user.id;
-    data.email = req.user.emails[0].value;
-    data.phone = 3001112222;
+    const urlFront = process.env.URL_FRONT;
+    const userSchema = createUserSchema(req);
     
-    const user = new User(data);
+    if (await User.findOne({ username: userSchema.username })) {
+      return res.redirect(301, urlFront); // If user is resgistered, redirect
+    }
 
     try {
+      const user = new User(data);
       await user.save();
     } catch (error) {
       console.log(error.message);
     }
 
-    const urlFront = process.env.URL_FRONT;
-
     res.redirect(301, urlFront);
   }
 );
+
+function createUserSchema(req) {
+  return {
+    name: req.user.displayName,
+    username: "google" + req.user.id.slice(1, 7),
+    password: req.user.id,
+    email: req.user.emails[0].value,
+    phone: 3001112222,
+  }
+}
 
 module.exports = router;
