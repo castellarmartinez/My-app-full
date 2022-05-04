96151171c57a6c7dc30a5ae84a959a3f4fca8419
David Castellar Martínez <castellardav@gmail.com>
2022-05-03 22:53:30 -0500

    Allows to login with linkedin

--- backend/src/routes/auth/linkedin.js
+++ backend/src/routes/auth/linkedin.js
@@ -21,31 +21,33 @@ router.get(
     session: false,
     failureRedirect: "/failed",
   }),
- async  (req, res) => {
-    console.log(`Peticion get /${strategyName}/callback`);
+  async (req, res) => {
+    const urlFront = process.env.URL_FRONT;
+    const userSchema = createUserSchema(req);
 
-    let data = {};
-    
-    data.name = req.user.displayName;
-    data.username = "linkedin" + req.user.id.slice(1, 7);
-    data.password = req.user.id;
-    data.email = req.user.id.slice(1, 7) + "@delilahresto.com";
-    data.phone = 3001112222;
-    
-    console.log(req.user);
-    console.log("Data", data);
-    const user = new User(data);
+    if (await User.findOne({ username: userSchema.username })) {
+      return res.redirect(301, urlFront); // If user is resgistered, redirect
+    }
 
     try {
+      const user = new User(data);
       await user.save();
     } catch (error) {
       console.log(error.message);
     }
 
-    const urlFront = process.env.URL_FRONT;
-
     res.redirect(301, urlFront);
   }
 );
+
+function createUserSchema(req) {
+  return {
+    name: req.user.displayName,
+    username: "linkedin" + req.user.id.slice(1, 7),
+    password: req.user.id,
+    email: req.user.id.slice(1, 7) + "@delilahresto.com",
+    phone: 3001112222,
+  };
+}
 
 module.exports = router;
